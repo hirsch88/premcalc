00a2e1f6d33fdcce093d10074b81c3b6f9442093
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-03-12 09:39:55 +0100

    add storybook

--- libs/insurance-taker/src/lib/insurance-taker.component.ts
+++ libs/insurance-taker/src/lib/insurance-taker.component.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-insurance-taker',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>insurance-taker works!</p>`,
+  template: `<p>insurance-taker works! Bubu adsf</p>`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InsuranceTakerComponent {}
+export class InsuranceTakerComponent {
+  /**
+   * Lastname gugus adsf
+   */
+  @Input() name: string = '';
+  /**
+   * Firstname gugus
+    */
+  @Input() fname: string = '';
+}
